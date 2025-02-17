a0d1e79f41b00dad72a2008ce627ae4874717838
Leonardo-de-Moura <francisco.tico.fssm@gmail.com>
2025-02-17 19:28:50 -0300

    up

--- routes/route.py
+++ routes/route.py
@@ -5,7 +5,7 @@ livros_routes= Blueprint('livros', __name__)
 
 @livros_routes.route('/livros', methods=['GET'])
 def obter_livros():
-	pass
+	return jsonify(Livros)
 
 @livros_routes.route('/livros/<int:id>', methods= ['GET'])
 def obter_livros_id(id):
